77580814999b419d33a022cfc09924783f23a0f0
Jhones <jhonesaly@gmail.com>
2023-10-31 16:58:17 -0300

    refactor: apagando partes

--- main_functions.py
+++ main_functions.py
@@ -3,6 +3,42 @@
 import random
 from main_classes import Hero
 
+def lvl_info(hero_xp):
+    if hero_xp < 1000:
+        return "Ferro"
+    elif 1001 <= hero_xp <= 2000:
+        return "Bronze"
+    elif 2001 <= hero_xp <= 5000:
+        return "Prata"
+    elif 5001 <= hero_xp <= 7000:
+        return "Ouro"
+    elif 7001 <= hero_xp <= 8000:
+        return "Platina"
+    elif 8001 <= hero_xp <= 9000:
+        return "Ascendente"
+    elif 9001 <= hero_xp <= 10000:
+        return "Imortal"
+    else:
+        return "Radiante"
+
+
+def rating_info(hero_rating):
+    if hero_rating < 10:
+        return "Ferro"
+    elif 11 <= hero_rating <= 20:
+        return "Bronze"
+    elif 21 <= hero_rating <= 50:
+        return "Prata"
+    elif 51 <= hero_rating <= 80:
+        return "Ouro"
+    elif 81 <= hero_rating <= 90:
+        return "Diamante"
+    elif 91 <= hero_rating <= 100:
+        return "Lendário"
+    else:
+        return "Imortal"
+
+
 def random_name():
     consoantes = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z']
     vogais = ['a', 'e', 'i', 'o', 'u']
@@ -38,6 +74,7 @@ def random_hero():
     hero = Hero(hero_name, hero_age, hero_profession, hero_xp)
     return hero
 
+
 def random_hero_league(hero_quantity):
     hero_league = []
     for i in range(hero_quantity):
@@ -47,30 +84,10 @@ def random_hero_league(hero_quantity):
     return hero_league
 
 
-def lvl_info(hero_xp):
-    if hero_xp < 1000:
-        return "Ferro"
-    elif 1001 <= hero_xp <= 2000:
-        return "Bronze"
-    elif 2001 <= hero_xp <= 5000:
-        return "Prata"
-    elif 5001 <= hero_xp <= 7000:
-        return "Ouro"
-    elif 7001 <= hero_xp <= 8000:
-        return "Platina"
-    elif 8001 <= hero_xp <= 9000:
-        return "Ascendente"
-    elif 9001 <= hero_xp <= 10000:
-        return "Imortal"
-    else:
-        return "Radiante"
-
-
 def show_heroes(hero_league):
-    print("Hero\tOfício\tXP\tVictory\tDefeat\tRatio")
+    print("Hero\tIdade\tOfício\tXP\tVictory\tDefeat\tRatio")
     for hero in hero_league:
-        print(f"{hero.name}\t{hero.profession}\t{hero.xp}\t{hero.victories}\t{hero.defeats}\t{hero.victories-hero.defeats}")
-
+        print(f"{hero.name}\t{hero.age}\t{hero.profession}\t{hero.xp}\t{hero.victories}\t{hero.defeats}\t{hero.victories-hero.defeats}")
 
 
 def hero_match(hero_1, hero_2):
@@ -79,7 +96,7 @@ def hero_match(hero_1, hero_2):
         'ladino': 'mago',
         'mago': 'soldado'
     }
-    
+
     hero_1_xp = hero_1.xp
     hero_2_xp = hero_2.xp
 
@@ -102,6 +119,7 @@ def hero_match(hero_1, hero_2):
         hero_1.defeats += 1
         return hero_2.name
 
+
 def hero_championship(hero_league, seasons=1):
     for k in range(seasons):
         for i in range(len(hero_league)):
@@ -112,26 +130,11 @@ def hero_championship(hero_league, seasons=1):
                     hero_match(hero_league[i], hero_league[j])
 
 
-def rating_info(hero_rating):
-    if hero_rating < 10:
-        return "Ferro"
-    elif 11 <= hero_rating <= 20:
-        return "Bronze"
-    elif 21 <= hero_rating <= 50:
-        return "Prata"
-    elif 51 <= hero_rating <= 80:
-        return "Ouro"
-    elif 81 <= hero_rating <= 90:
-        return "Diamante"
-    elif 91 <= hero_rating <= 100:
-        return "Lendário"
-    else:
-        return "Imortal"
-
 if __name__ == '__main__':
 
     import os
     os.system('cls')
 
     hero_league = random_hero_league(20)
+    hero_championship(hero_league, 4)
     show_heroes(hero_league)
